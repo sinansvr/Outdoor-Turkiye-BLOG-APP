2f51739b48128ad17f9c8c686ea460ef41a209b0
sinansvr <svrsinan06@gmail.com>
2025-07-08 23:58:52 +0300

    index

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ require("dotenv").config()
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
 
+// To accept JSON
 app.use(express.json())
 
+//Home path
 app.all("/", (req, res)=>{
 
     res.status(200).send({
@@ -16,6 +18,7 @@ app.all("/", (req, res)=>{
     })
 })
 
+// errorHandler
 app.use(require("./middlewares/errorHandler"))
 
 app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
