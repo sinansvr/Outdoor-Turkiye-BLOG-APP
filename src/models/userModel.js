14cfc780a631d303d953b0c3b308588ef5876c29
sinansvr <svrsinan06@gmail.com>
2025-07-09 15:24:13 +0300

    up

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -50,10 +50,36 @@ const UserSchema = new mongoose.Schema({
 
 }, { collection: "users", timestamps: true })
 
-const passwordEncrypt = require("mongoose-bcrypt")
+const passwordEncrypt = require("../helpers/passwordEncrypt")
 
 UserSchema.pre("save","update", function (next){
+    const data = this?.update || this
 
-} )
+    const isEmailValidated = data?.email ?  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email): true
 
+    if (isEmailValidated) {
+
+        if (data?.password) {
+
+            // pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
+            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
+
+            if (isPasswordValidated) {
+
+                this.password = data.password = passwordEncrypt(data.password)
+                this._update = data // updateOne will wait data from "this._update".
+
+            } else {
+
+                next(new Error('Password not validated.'))
+            }
+        }
+
+        next() // Allow to save.
+
+    } else {
+
+        next(new Error('Email not validated.'))
+    }
+})
 module.exports = mongoose.model("User", UserSchema)
