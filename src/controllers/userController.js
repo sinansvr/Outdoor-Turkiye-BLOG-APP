b99e167f48837e9f25d7a743c901ebbd38520e80
sinansvr <svrsinan06@gmail.com>
2025-07-18 23:54:10 +0300

    up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,31 @@
 "use strict"
+
+const User = require("../models/userModel")
+const passwordEncrypt = require("../helpers/passwordEncrypt")
+
+module.exports = {
+    list: async (req, res) => {
+        const data = await User.find()
+        res.status(200).send({
+            error: false,
+            data})
+    },
+
+    create: async (req, res)=>{
+        const data = User.create()
+
+    },
+
+    read : async (req, res) => {
+
+    },
+
+    update : async (req, res)=> {
+
+    },
+
+    delete : async (req, res) => {
+
+    }
+
+}
