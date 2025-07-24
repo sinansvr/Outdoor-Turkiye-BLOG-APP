56627f44c5468a7caaa1c16dce1a8f2120afab73
sinansvr <svrsinan06@gmail.com>
2025-07-24 22:54:58 +0300

    user controller finished

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,12 +11,13 @@ module.exports = {
         res.status(200).send({
             error: false,
             data,
-            details:res.getModelListDetails(User)
+            details: res.getModelListDetails(User)
         })
     },
 
     create: async (req, res) => {
 
+        // disallow to be admin
         req.body.isAdmin = false;
 
         const data = await User.create(req.body)
@@ -30,16 +31,48 @@ module.exports = {
 
     read: async (req, res) => {
 
-        const data = await User.findOne({_id:req.params._id})
+
+        // the user only read self record 
+        // let filters={}
+
+        // if (req.user?.isAdmin) filters = {_id:req.user._id}
+
+        const data = await User.findOne({ _id: req.params._id })
+
+        res.status(200).send({
+            error: false,
+            data
+        })
 
     },
 
     update: async (req, res) => {
 
+        // let filters = {};
+
+        // if (req.user?.isAdmin){
+        //     filters = {_id:req.prams._id}
+        //     req.body.isAdmin= false;
+        // }
+
+        const data = await User.updateOne({ _id: req.params._id })
+
+        res.status(201).send({
+            error: false,
+            data,
+            new: await User.findOne({ _id: req.params._id })
+        })
+
     },
 
     delete: async (req, res) => {
 
+        const data = await User.deleteOne({_id:req.params._id})
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error:!data.deletedCount,
+            data
+        })
     }
 
 }
