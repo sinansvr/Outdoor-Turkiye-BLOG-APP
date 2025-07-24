a9aae3414cc950dc74af7b89879b8a50fa3f66db
sinansvr <svrsinan06@gmail.com>
2025-07-24 22:28:08 +0300

    up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,26 +5,40 @@ const passwordEncrypt = require("../helpers/passwordEncrypt")
 
 module.exports = {
     list: async (req, res) => {
-        const data = await User.find()
+        // const data = await User.find()
+
+        const data = await res.getModelList(User)
         res.status(200).send({
             error: false,
-            data})
+            data,
+            details:res.getModelListDetails(User)
+        })
     },
 
-    create: async (req, res)=>{
-        const data = User.create()
+    create: async (req, res) => {
+
+        req.body.isAdmin = false;
+
+        const data = await User.create(req.body)
+
+        res.status(200).send({
+            error: false,
+            data
+        })
 
     },
 
-    read : async (req, res) => {
+    read: async (req, res) => {
+
+        const data = await User.findOne({_id:req.params._id})
 
     },
 
-    update : async (req, res)=> {
+    update: async (req, res) => {
 
     },
 
-    delete : async (req, res) => {
+    delete: async (req, res) => {
 
     }
 
